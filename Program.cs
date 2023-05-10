ca76d0b9900b6d10bf9ee58b230399e2d6029fe8
Leo-Martins-Dev <leonardomartins578@gmail.com>
2023-05-10 17:33:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1 = Segundo => 10s = 10 segundos");
+            Console.WriteLine("2 = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+
+        }
+
+        static void Start(int time)
+        {
+           
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cronômetro finalizado");
+            Thread.Sleep(2000);
+        }
+    }
+}
